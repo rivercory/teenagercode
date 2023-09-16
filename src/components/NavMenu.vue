7373c3d7792c02b09424e0dff99b4ec2b75f9abd
hyeonho64 <hyeonhokang10@kakao.com>
2023-09-16 20:59:36 +0900

    Version 1.1.2

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="top-bar rounded-2 shadow-sm bg-body-tertiary" aria-label="Light offcanvas navbar">
+    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight"
+            aria-controls="offcanvasNavbarLight" aria-label="Toggle navigation">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+    </button>
+    <a class="btn navbar-brand" href="#">{{ title }}</a>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight"
+         aria-labelledby="offcanvasNavbarLightLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+               aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+.top-bar {
+  padding: 1rem;
+  margin: 1rem;
+}
+
+.navbar-brand {
+  font-weight: bold;
+  font-family: "SUIT Light";
+  font-size: 1.4rem;
+}
+</style>
+
+<script>
+export default {
+  props: ["title"],
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <NavMenu title="틴에이저코드"/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavMenu from "@/components/NavMenu.vue";
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {NavMenu}
 }
 </script>
+
+<style>
+</style>
