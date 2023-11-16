7b48a96cc97d972ed38a1bd1eddf91250f71bf11
hyeonho64 <hyeonhokang10@kakao.com>
2023-11-16 11:55:38 +0900

    Version 1.2.5

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './components/css/App.css';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <div className="h-screen">
+      <div>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
-      <Footer />
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,27 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>홈</h1>
-      <p>가장 먼저 보여지는 페이지입니다.</p>
-    </div>
-  );
-};
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://pwwwklfyytziqvrhgebq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3d3drbGZ5eXR6aXF2cmhnZWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ5MjU4NzQsImV4cCI6MjAxMDUwMTg3NH0.6fUasoQ8weL-_4PuCFLTpOJ7PiknFM8KbY5fVvrKLwo");
+
+function Home() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        getCountries();
+    }, []);
+
+    async function getCountries() {
+        const { data } = await supabase.from("countries").select();
+        setCountries(data);
+    }
+
+    return (
+        <ul>
+            {countries.map((country) => (
+                <li key={country.name}>{country.name}</li>
+            ))}
+        </ul>
+    );
+}
 
 export default Home;
