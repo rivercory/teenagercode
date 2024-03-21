5e6db6ab4b603b1ada0ba949c571eb49b2102c6f
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-03-21 21:40:07 +0900

    Version 1.4.2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {IoMdSettings, IoMdNotifications} from "react-icons/io";
 
 const Navbar = () => {
     return (
-        <div className="w-auto navbar">
+        <div className="w-auto navbar bg-gradient-to-r from-primary to-secondary">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
